42f11b85f53aac2c13dbd29b05f6117786061e5d
jobala <japhethobalak@gmail.com>
2018-08-30 15:53:56 +0300

    Adds authentication flow

---
+++ Calendar/Authentication.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace Helpers
+{
+    class Authentication
+    {
+        private static string clientId = ConfigurationManager.AppSettings["clientId"].ToString();
+        private static string[] scopes = {
+            "User.Read"
+        };
+
+        private static PublicClientApplication identityClientApp = new PublicClientApplication(clientId);
+        private static GraphServiceClient graphClient = null;
+
+        public static GraphServiceClient GetAuthenticatedClient()
+        {
+            if (graphClient == null)
+            {
+                try
+                {
+                    graphClient = new GraphServiceClient(
+                        "https://graph.microsoft.com",
+                        new DelegateAuthenticationProvider(
+                                async (requestMessage) =>
+                                {
+                                    var token = await getTokenForUserAsync();
+                                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                                }
+                            ));
+                    return graphClient;
+                } 
+                catch(Exception error)
+                {
+                    Debug.WriteLine($"Could not create a graph client {error.Message}");
+                }
+            }
+            return graphClient;
+        }
+
+        private static async Task<string> getTokenForUserAsync()
+        {
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                IEnumerable<IAccount> account = await identityClientApp.GetAccountsAsync();
+                Console.WriteLine(account.GetType());
+                authResult = await identityClientApp.AcquireTokenSilentAsync(scopes, account as IAccount);
+                return authResult.AccessToken;
+            }
+            catch(MsalUiRequiredException error)
+            {
+                authResult = await identityClientApp.AcquireTokenAsync(scopes);
+                return authResult.AccessToken;
+            }
+        }
+
+    }
+}
--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Helpers;
+using Microsoft.Graph;
+using System.Diagnostics;
 
 namespace Calendar
 {
@@ -11,9 +11,44 @@ namespace Calendar
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            RunAsync().GetAwaiter().GetResult();
             Console.ReadKey();
+        }
 
+        public static async Task<User> GetMeAsync()
+        {
+            User currentUserObject = null;
+            try
+            {
+                var graphClient = Authentication.GetAuthenticatedClient();
+                currentUserObject = await graphClient.Me.Request().GetAsync();
 
+                Debug.WriteLine("Got user: " + currentUserObject.DisplayName);
+                return currentUserObject;
+            }
+
+            catch (ServiceException e)
+            {
+                Debug.WriteLine("We could not get the current user: " + e.Error.Message);
+                return null;
+            }
+        }
+
+        static async Task RunAsync()
+        {
+            //Display information about the current user
+            Console.WriteLine("Get My Profile");
+            Console.WriteLine();
+
+            var me = await GetMeAsync();
+
+            Console.WriteLine(me.DisplayName);
+            Console.WriteLine("User:{0}\t\tEmail:{1}", me.DisplayName, me.Mail);
+            Console.WriteLine();
+
+            //Display information about people near me
+            Console.WriteLine("Get People Near Me");
         }
     }
 }
