3ba0446ec9283b8c8bf528a3b4dd2896e2485aa0
jobala <japhethobalak@gmail.com>
2018-09-11 06:33:09 +0300

    Adds microsoft graph

--- Calendar/CalendarController.cs
+++ Calendar/CalendarController.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Graph;
 
 namespace Calendar
 {
